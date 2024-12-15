302460d03c9c22062adca5d89ca138d7424fe436
tamimhasan19702 <tamimhasancu@gmail.com>
2024-12-15 23:25:07 +0600

    update menu.php

--- template-parts/header-parts/menu.php
+++ template-parts/header-parts/menu.php
@@ -8,25 +8,35 @@
 </button>
 
 <div class="collapse navbar-collapse" id="navbarCollapse">
-    <div class="navbar-nav ms-auto py-0">
-        <a href="index.html" class="nav-item nav-link active">Home</a>
-        <a href="about.html" class="nav-item nav-link">About</a>
-        <a href="service.html" class="nav-item nav-link">Services</a>
-        <a href="blog.html" class="nav-item nav-link">Blogs</a>
-        <div class="nav-item dropdown">
+    <ul class="navbar-nav ms-auto py-0">
+        <li class="nav-item">
+            <a href="index.html" class="nav-link active">Home</a>
+        </li>
+        <li class="nav-item">
+            <a href="about.html" class="nav-link">About</a>
+        </li>
+        <li class="nav-item">
+            <a href="service.html" class="nav-link">Services</a>
+        </li>
+        <li class="nav-item">
+            <a href="blog.html" class="nav-link">Blogs</a>
+        </li>
+        <li class="nav-item dropdown">
             <a href="#" class="nav-link" data-bs-toggle="dropdown">
                 <span class="dropdown-toggle">Pages</span>
             </a>
-            <div class="dropdown-menu m-0">
-                <a href="feature.html" class="dropdown-item">Our Features</a>
-                <a href="team.html" class="dropdown-item">Our team</a>
-                <a href="testimonial.html" class="dropdown-item">Testimonial</a>
-                <a href="offer.html" class="dropdown-item">Our offer</a>
-                <a href="FAQ.html" class="dropdown-item">FAQs</a>
-                <a href="404.html" class="dropdown-item">404 Page</a>
-            </div>
-        </div>
-        <a href="contact.html" class="nav-item nav-link">Contact Us</a>
-    </div>
+            <ul class="dropdown-menu m-0">
+                <li><a href="feature.html" class="dropdown-item">Our Features</a></li>
+                <li><a href="team.html" class="dropdown-item">Our team</a></li>
+                <li><a href="testimonial.html" class="dropdown-item">Testimonial</a></li>
+                <li><a href="offer.html" class="dropdown-item">Our offer</a></li>
+                <li><a href="FAQ.html" class="dropdown-item">FAQs</a></li>
+                <li><a href="404.html" class="dropdown-item">404 Page</a></li>
+            </ul>
+        </li>
+        <li class="nav-item">
+            <a href="contact.html" class="nav-link">Contact Us</a>
+        </li>
+    </ul>
     <a href="#" class="btn btn-primary rounded-pill py-2 px-4 my-3 my-lg-0 flex-shrink-0">Get Started</a>
 </div>
